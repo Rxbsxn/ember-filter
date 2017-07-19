27db199d2c1838b975e5e4ef7d0f0d2bb2841c79
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-19 12:21:08 +0200

    tdd test for filter

---
+++ app/components/ember-filter/filter-list/filter/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-filter/components/ember-filter/filter-list/filter/component';
---
+++ tests/integration/components/ember-filter/filter-list/filter/component-test.js
@@ -0,0 +1,15 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('ember-filter/filter-list/filter', 'Integration | Component | ember filter/filter list/filter', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  let filter = { name: 'super filter', value: 3, type: 'project' }
+  this.set('filter', filter);
+
+  this.render(hbs`{{ember-filter/filter-list/filter filter=filter}}`);
+
+  assert.equal(this.$('').text().trim(), 'super filter');
+});
