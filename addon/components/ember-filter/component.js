eb33448880377c6f015ae8953b2367c50d4c4a85
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-21 09:55:28 +0200

    uniq list prototype

--- addon/components/ember-filter/component.js
+++ addon/components/ember-filter/component.js
@@ -10,16 +10,21 @@ export default Component.extend({
     return A(this.get('filters'));
   }),
 
+  uniqueList: computed.uniqBy('arrayFilters', 'type'),
+
   availableFilters: A([{name: "State", values: {0: 'Done', 1: 'WIP'}, type: 'project'},
                     {name: 'Workers', values: {0: 'Min', 1: 'Max'}, type: 'work'}]),
 
   actions: {
     addFilter(filter) {
-      this.get('arrayFilters').pushObject(filter);
-    },
-
-    removeFromAvailables(filter) {
-      this.get('availableFilters').removeObject(filter);
+      let arrayFilters = this.get('arrayFilters');
+      let sameTypeRecord = arrayFilters.findBy('type', filter.type);
+      if(sameTypeRecord) {
+        arrayFilters.removeObject(sameTypeRecord);
+        arrayFilters.pushObject(filter);
+      } else {
+        arrayFilters.pushObject(filter);
+      }
     }
   }
 });
