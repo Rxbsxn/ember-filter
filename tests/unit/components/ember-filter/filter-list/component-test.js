fa241ef2ac380593f5e4f2c1da29726be5dac6b5
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-24 07:51:05 +0200

    remove filter test

---
+++ tests/unit/components/ember-filter/filter-list/component-test.js
@@ -0,0 +1,31 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember'
+
+const { A } = Ember;
+
+moduleForComponent('ember-filter/filter-list', 'Unit | Component | ember filter/filter list', {
+    unit: true,
+    beforeEach() {
+        let component = this.subject();
+        let filters = A([{name: "Open", value: 0, type: "project"}, {name: 'X', value: 1, type: "qaz"}]);
+        component.set('filters', filters);
+    }
+});
+
+test('it renders', function(assert) {
+    let component = this.subject();
+
+    assert.ok(component);
+});
+
+test('it removes filter from list', function(assert) {
+    let filter = { name: "Close", value: 1, type: 'work' }
+    let component = this.subject();
+
+    component.get('filters').pushObject(filter);
+    assert.equal(component.get('filters').length, 3);
+
+    component.send('removeFilter', filter);
+
+    assert.equal(component.get('filters').length, 2);
+});
